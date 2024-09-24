1e9d6043b346253e5c3794bffee73f5c0f8eff51
donpj <gigipiscitelli@gmail.com>
2024-09-24 19:00:01 +0100

    SSL not working

--- supabase/functions/revolut-oauth/index.ts
+++ supabase/functions/revolut-oauth/index.ts
@@ -1,22 +1,20 @@
 import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@1.30.0";
 
-console.log("Starting the function...");
-
+// Load environment variables
 const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
 const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 const revolutClientId = Deno.env.get("REVOLUT_CLIENT_ID")!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Key:", supabaseKey);
 console.log("Revolut Client ID:", revolutClientId);
-console.log("Environment Variables:", Deno.env.toObject());
 
 serve(async (req: Request) => {
   try {
-    console.log("Parsing request body...");
+    console.log("Request received");
     const { userId } = await req.json();
+    console.log("User ID:", userId);
 
     if (!userId) {
       console.log("Missing userId");
@@ -41,8 +39,17 @@ serve(async (req: Request) => {
       },
     );
 
+    console.log("Response Status: ", clientTokenResponse.status);
+    if (!clientTokenResponse.ok) {
+      console.error("Error fetching token:", clientTokenResponse.statusText);
+      return new Response(
+        JSON.stringify({ error: "Failed to fetch token" }),
+        { status: clientTokenResponse.status },
+      );
+    }
+
     const clientTokenData = await clientTokenResponse.json();
-    console.log("Received client token response", clientTokenData);
+    console.log("Revolut Token Response:", clientTokenData);
     const accessToken = clientTokenData.access_token;
 
     if (!accessToken) {
@@ -59,14 +66,14 @@ serve(async (req: Request) => {
       {
         method: "POST",
         headers: {
-          "x-fapi-financial-id": "001580000103UAvAAM",
           Authorization: `Bearer ${accessToken}`,
+          "x-fapi-financial-id": "001580000103UAvAAM",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
           Data: {
             Permissions: ["ReadAccountsBasic", "ReadAccountsDetail"],
-            ExpirationDateTime: "2024-12-02T00:00:00+00:00",
+            ExpirationDateTime: "2024-12-31T00:00:00+00:00",
             TransactionFromDateTime: "2024-01-01T00:00:00+00:00",
             TransactionToDateTime: "2024-12-31T00:00:00+00:00",
           },
@@ -75,8 +82,16 @@ serve(async (req: Request) => {
       },
     );
 
+    if (!consentResponse.ok) {
+      console.error("Error creating consent:", consentResponse.statusText);
+      return new Response(
+        JSON.stringify({ error: "Failed to create consent" }),
+        { status: consentResponse.status },
+      );
+    }
+
     const consentData = await consentResponse.json();
-    console.log("Consent response data:", consentData);
+    console.log("Consent Response Data:", consentData);
     const consentId = consentData?.Data?.ConsentId;
 
     if (!consentId) {
